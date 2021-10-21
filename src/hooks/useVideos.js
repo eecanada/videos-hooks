23ae56d7d8ab4abecffc7e9b21a4c96701b49eab
Eder Canada <canada.eder@gmail.com>
2021-10-21 10:39:22 -0700

    remove react from useVideos

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import youtube from '../apis/youtube'
+import { useState, useEffect } from 'react';
+import youtube from '../apis/youtube';
 
 const useVideos = (defaultSearchTerm) => {
   const [videos, setVideos] = useState([]);
